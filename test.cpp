aff14f14308aa2885bd5261aa8bc811c8c749298
GitHubharuya <2hareruya1016@gmail.com>
2025-10-05 23:30:02 +0900

    二重振り子軌道に関してのプログラム関連

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define rep(i,n) for(ll i=0;i<(n);i++)
+#define drep(i,n) for(ll i=(n)-1;i>=0;i--)
+#define rrep(i,n) for(ll i=1;i<(n);i++)
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define YN {cout << "Yes" << endl;}else{cout << "No" << endl;}
+#define dame cout << -1 << endl
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define vout(a) {rep(i,(int)a.size()){cout << a[i] << ' ';} cout << endl;}
+#define vvout(a) rep(i,(int)a.size()){{rep(j,(int)a[i].size()) cout << a[i][j] << ' ';}cout << '\n';}
+void chmax(auto& x,auto y){x = max(x,y);}
+void chmin(auto& x,auto y){x = min(x,y);}
+#define INF 2e9
+#define INFL 2e18
+void pirntf(ld x) {printf("%.10Lf\n",x);}
+vector<int> dx = {1,0,-1,0};
+vector<int> dy = {0,1,0,-1};
+#define Mod 998244353
+using P = pair<int,int>;
+bool seg(auto a,auto b,auto x,auto y){return (a<0 || a>=x || b<0 || b>=y);}
+
+int main(){
+  
+}
