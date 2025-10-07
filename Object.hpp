b2237fb91558933b756bcfb4ccf9a92f09180958
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-10-07 23:19:30 +0900

    Add: Object make side faces from slices
    側面のデータを作成する.
    スライスの座標は反時計回りであることを仮定する.
    `std::vector<std::array<PointIdx, 3>> rect_face(PointIdx a, PointIdx b, PointIdx c, PointIdx d)`
    は4点から向きの揃った2つの三角形のペアを返す.

--- Object.hpp
+++ Object.hpp
@@ -16,7 +16,43 @@ struct Object {
     bool check_slice_point_size() const ;
     bool from_slices();
     bool make_points_from_slices();
+    bool make_faces_from_slices();
 };
+
+std::vector<std::array<PointIdx, 3>> rect_face(PointIdx a, PointIdx b, PointIdx c, PointIdx d) {
+    return {std::array{a, b, c}, std::array{c, d, a}};
+}
+
+bool Object::make_faces_from_slices() {
+    int slice_size = slices.size();
+    if (slice_size == 0) {
+        return false;
+    }
+    int slice_point_size = slices[0].points.size();
+    faces.clear();
+    faces.reserve((slice_size-1) * slice_point_size * 2); // 各側面につき三角形2枚
+    for (int t = 1; t < slice_size; t++) {
+        PointIdx cur_offset = t * slice_point_size;
+        PointIdx pre_offset = (t-1) * slice_point_size;
+        for (int i = 1; i < slice_point_size; i++) {
+            auto face = rect_face(
+                 cur_offset + i, cur_offset + i-1, // 今の点
+                 pre_offset + i-1, pre_offset + i // 一つ前の点
+            );
+            faces.insert(faces.end(), face.begin(), face.end());
+        }
+        auto face = rect_face(
+             cur_offset + 0, cur_offset + slice_point_size-1, // 今の点
+             pre_offset + slice_point_size-1, pre_offset + 0 // 一つ前の点
+        );
+        faces.insert(faces.end(), face.begin(), face.end());
+    }
+
+    // t = 0 と t = slice_size の面をふさぐ
+    // make_begin_and_end_face();
+
+    return true;
+}
 
 bool Object::make_points_from_slices() {
     points.clear();
