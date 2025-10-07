0b7bf94b264413023416bfdb86ec34fdd0cdf1de
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-10-07 22:22:47 +0900

    Add: Slice input operator
    `t, x0 y0 x1 y1 ... xn yn\n` という形式の入力を受け取る.
    注意: 末尾に空白があると失敗することがある.

--- Slice.hpp
+++ Slice.hpp
@@ -13,3 +13,36 @@ struct Slice {
     void sort_by_direction();
 };
 
+ // 形式: t, x0 y0 x1 y1 ... xn yn'\n'
+ // WARNING: 行末に空白があると失敗することがある.
+std::istream& operator>>(std::istream& ist, Slice& slice) {
+    if (!(ist >> slice.t) || ist.get() != ',') {
+        // t とカンマの読み込みに失敗
+        ist.setstate(std::ios_base::failbit);
+        return ist;
+    }
+
+    slice.points.clear();
+
+    double x, y;
+    while (ist.peek() != '\n' && ist.peek() != EOF) {
+        if (ist >> x >> y) {
+            slice.points.emplace_back(Slice::Point2{x, y});
+        } else {
+            // 点の座標の読み込みに失敗
+            ist.setstate(std::ios_base::failbit);
+            break;
+        }
+    }
+
+    // ストリームが正常な場合、行の残りを読み飛ばす
+    if (ist) {
+        ist.ignore(
+            std::numeric_limits<std::streamsize>::max(),
+            '\n'
+        );
+    }
+
+    return ist;
+}
+
