6c5bee7665642c3a5c0e338d39f803da35d0acc1
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-10-07 23:02:21 +0900

    Add: Object output operator
    `Object` 型の点と面のデータをOBJ形式で出力する.

--- Object.hpp
+++ Object.hpp
@@ -14,3 +14,31 @@ struct Object {
     std::vector<Slice> slices;
 };
 
+std::istream& operator>>(std::istream& ist, Object& obj) {
+    Slice slice;
+    while (ist >> slice) {
+        // slice.sort_by_direction();
+        obj.slices.push_back(slice);
+    }
+
+    if (ist.fail() && !ist.eof()) {
+        return ist;
+    }
+    if (!obj.check_slice_point_size() || !obj.from_slices()) {
+        // スライスからの生成に失敗
+        ist.setstate(std::ios_base::failbit);
+    };
+    return ist;
+}
+
+std::ostream& operator<<(std::ostream& ost, const Object& obj) {
+    for (const auto& p : obj.points) {
+        ost << "v " << p.x << " " << p.y << " " << p.z << "\n";
+    }
+    for (const auto& f : obj.faces) {
+        // WARNING: 1-based indexing for OBJ format
+        ost << "f " << f[0] + 1 << " " << f[1] + 1 << " " << f[2] + 1 << "\n";
+    }
+    return ost;
+}
+
