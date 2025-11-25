b5e65ed31e2acacca2d36dd36195cf2bd3490b9b
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-11-25 18:19:46 +0900

    Add: LayerObj3D: 線形補間付き make_points
    各層の間に追加する層の個数を引数とし, 線形補間した点を追加する

--- LayerObj3D.hpp
+++ LayerObj3D.hpp
@@ -33,6 +33,44 @@ bool LayerObj3D::make_points() {
     return true;
 }
 
+bool LayerObj3D::make_points(int interpolate_size) {
+    // 最後の層以外の間で interpolate_size 個の層が追加される
+    PointSize exact_slice_size = (slices.size() - 1) * (interpolate_size + 1) + 1;
+    PointSize total_point_size = exact_slice_size * slices[0].points.size();
+    points.clear();
+    points.reserve(total_point_size);
+    double length_per_time = TOTAL_SIZE / exact_slice_size;
+
+    double z = 0;
+    // 最後の層を除き, 補間しながら点を追加する
+    for (PointSize i = 0; i < slices.size() - 1; i++) {
+        for (int polate_rate = 0; polate_rate < interpolate_size + 1; polate_rate++) {
+            for (PointSize j = 0; j < slices[i].points.size(); j++) {
+                Geom::Point2 p = slices[i].points[j];
+                Geom::Point2 q = slices[i + 1].points[j];
+                double t = (double)polate_rate / (interpolate_size + 1);
+                double x = p.x * (1 - t) + q.x * t;
+                double y = p.y * (1 - t) + q.y * t;
+                points.emplace_back(Geom::Point3{
+                    x, y, z
+                });
+            }
+            z += length_per_time;
+        }
+    }
+
+    // 最後の層はそのまま追加
+    for (const auto& p : slices.back().points) {
+        points.emplace_back(Geom::Point3{
+            p.x, p.y, z
+        });
+    }
+
+    this->step_size = exact_slice_size;
+
+    return true;
+}
+
 bool LayerObj3D::check_slice_point_size() const {
     for (const auto& slice : slices) {
         if (slice.points.size() != point_size_per_step) return false;
