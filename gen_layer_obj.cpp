844f87e7c4b30fc72ce577a87f98d77d58c21048
ayamoTake <ayamo1013takeuchi@gmail.com>
2025-12-18 10:58:06 +0900

    Change: gen_layer_obj の第三引数として補間回数を指定

--- gen_layer_obj.cpp
+++ gen_layer_obj.cpp
@@ -3,10 +3,14 @@
 
 int main(int argc, char** argv) {
     double length_par_time = 1;
+    int interpolate_size = 0;
     if (argc >= 2) {
         length_par_time = std::stof(argv[1]);
     }
-    LayerObj3D obj(length_par_time);
+    if (argc >= 3) {
+        interpolate_size = std::stoi(argv[2]);
+    }
+    LayerObj3D obj(length_par_time, interpolate_size);
     std::cin >> obj;
     std::cout << obj;
 }
